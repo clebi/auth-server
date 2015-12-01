9b6c13967075658e045547d487c68e298fd31014
Clément Bizeau <clement.bizeau4@gmail.com>
2015-12-02 00:39:33 +0100

    url /users/ shows current user infomration

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(res.json({
+    username: req.user.username,
+    createdAt: req.user.createdAt
+  }));
 });
 
 module.exports = router;
