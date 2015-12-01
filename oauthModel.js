903fc234702dd1446e3af79bb7f675bc6d55caa0
Clément Bizeau <clement.bizeau4@gmail.com>
2015-12-01 23:54:07 +0100

    use transactions

--- oauthModel.js
+++ oauthModel.js
@@ -1,4 +1,5 @@
 var models = require('./models'),
+  sequelize = require('sequelize'),
   model = module.exports;
 
 model.getAccessToken = function(bearerToken, callback) {
@@ -6,53 +7,57 @@ model.getAccessToken = function(bearerToken, callback) {
 };
 
 model.saveAccessToken = function (accessToken, clientId, expires, user, callback) {
-  models.OauthClient.findOne({
-    where: {
-      client_id: clientId
-    }
-  }).then(function(client) {
-    if(!client)
-      return callback("unable to find client");
-    return models.User.findOne({
+  models.sequelize.transaction(function(t) {
+    return models.OauthClient.findOne({
       where: {
-        user_id: user.id
+        client_id: clientId
       }
-    }).then(function(user) {
-      if(!user)
-        return callback("unable to find user");
-      return models.OauthAccessToken.create({
-        access_token: accessToken,
-        expires: expires
-      }).then(function(token) {
-        return client.addOauthAccessToken(token).then(function(client) {
-          return user.addOauthAccessToken(token).then(function(user) {
-            callback(false);
+    }, {transaction: t}).then(function(client) {
+      if(!client)
+        return callback("unable to find client");
+      return models.User.findOne({
+        where: {
+          user_id: user.id
+        }
+      }, {transaction: t}).then(function(user) {
+        if(!user)
+          return callback("unable to find user");
+        return models.OauthAccessToken.create({
+          access_token: accessToken,
+          expires: expires
+        }, {transaction: t}).then(function(token) {
+          return client.addOauthAccessToken(token, {transaction: t}).then(function(client) {
+            return user.addOauthAccessToken(token, {transaction: t}).then(function(user) {
+              callback(false);
+            });
           });
         });
       });
+    }).catch(function(error) {
+      callback(error);
     });
-  }).catch(function(error) {
-    callback(error);
   });
 };
 
 model.getClient = function(clientId, clientSecret, callback) {
-  models.OauthClient.findOne({
-    where: {
-      client_id: clientId,
-    }
-  }).then(function(oauthClient) {
-    if(!oauthClient)
-      return callback("unable to find client");
-    if(clientSecret !== null && oauthClient.client_secret !== clientSecret)
-      return callback("secret is wrong");
-    callback(null, {
-      clientId: oauthClient.client_id,
-      clientSecret: oauthClient.client_secret,
-      redirectUri: oauthClient.redirect_uri
+  models.sequelize.transaction(function(t) {
+    return models.OauthClient.findOne({
+      where: {
+        client_id: clientId,
+      }
+    }, {transaction: t}).then(function(oauthClient) {
+      if(!oauthClient)
+        return callback("unable to find client");
+      if(clientSecret !== null && oauthClient.client_secret !== clientSecret)
+        return callback("secret is wrong");
+      callback(null, {
+        clientId: oauthClient.client_id,
+        clientSecret: oauthClient.client_secret,
+        redirectUri: oauthClient.redirect_uri
+      });
+    }).catch(function(error) {
+      callback(error);
     });
-  }).catch(function(error) {
-    callback(error);
   });
 };
 
@@ -61,55 +66,58 @@ model.grantTypeAllowed = function (clientId, grantType, callback) {
 };
 
 model.getAuthCode = function(authCode, callback) {
-  models.OauthCode.findOne({
-    where: {
-      code: authCode
-    }
-  }).then(function(code) {
-    if(!code)
-      callback('unable to find auth code');
-    return code.getOauthClient().then(function(client) {
-      return code.getUser().then(function(user) {
-        callback(false, {
-          clientId: client.client_id,
-          expires: code.expires,
-          userId: user.user_id
+  models.sequelize.transaction(function(t) {
+    return models.OauthCode.findOne({
+      where: {
+        code: authCode
+      }
+    }, {transaction: t}).then(function(code) {
+      if(!code)
+        callback('unable to find auth code');
+      return code.getOauthClient({transaction: t}).then(function(client) {
+        return code.getUser({transaction: t}).then(function(user) {
+          callback(false, {
+            clientId: client.client_id,
+            expires: code.expires,
+            userId: user.user_id
+          });
         });
-      });
-    })
-  }).catch(function(error) {
-    callback(error);
+      })
+    }).catch(function(error) {
+      callback(error);
+    });
   });
 };
 
 model.saveAuthCode = function(authCode, clientId, expires, user, callback) {
-  models.OauthClient.findOne({
-    where: {
-      client_id: clientId,
-    }
-  }).then(function(client) {
-    if(!client)
-      callback("unable to find client");
-    return models.User.findOne({
+  models.sequelize.transaction(function(t) {
+    return models.OauthClient.findOne({
       where: {
-        user_id: user
+        client_id: clientId,
       }
-    }).then(function(user) {
-      if(!user)
-        callback("unable to find user");
-      return models.OauthCode.create({
-        code: authCode,
-        expires: expires
-      }).then(function(code) {
-        return client.addOauthCode(code).then(function(client) {
-          return user.addOauthCode(code).then(function(user) {
-            callback();
+    }, {transaction: t}).then(function(client) {
+      if(!client)
+        callback("unable to find client");
+      return models.User.findOne({
+        where: {
+          user_id: user
+        }
+      }, {transaction: t}).then(function(user) {
+        if(!user)
+          callback("unable to find user");
+        return models.OauthCode.create({
+          code: authCode,
+          expires: expires
+        }, {transaction: t}).then(function(code) {
+          return client.addOauthCode(code, {transaction: t}).then(function(client) {
+            return user.addOauthCode(code, {transaction: t}).then(function(user) {
+              callback();
+            });
           });
         });
       });
+    }).catch(function(error) {
+      callback(error);
     });
-  }).catch(function(error) {
-    console.log(error);
-    callback(error);
   });
 };
