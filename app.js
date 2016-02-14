9c3b89ef42c43bd72ea96bcf0fa77f4343f417c4
Clément Bizeau <clement.bizeau4@gmail.com>
2016-02-14 23:44:47 +0100

    send empty response on error

--- app.js
+++ app.js
@@ -35,8 +35,9 @@ app.use(function(req, res, next) {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
   loggers.error.error(err);
+  res.status(err.status || 500);
+  res.send();
 });
 
 module.exports = app;
